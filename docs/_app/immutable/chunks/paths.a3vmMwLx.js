e7ac6e40aaa06f9712059a0426a88e9a06a4f9ab
James Hamann <hamannjames87@gmail.com>
2024-01-17 15:57:27 -0800

    version with jot form

---
+++ docs/_app/immutable/chunks/paths.a3vmMwLx.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_zlwq8u?.base??"/kinder-painting",a=globalThis.__sveltekit_zlwq8u?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/paths.xrD2wPC6.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_9ypy48?.base??"/kinder-painting",a=globalThis.__sveltekit_9ypy48?.assets??s;export{a,s as b};
