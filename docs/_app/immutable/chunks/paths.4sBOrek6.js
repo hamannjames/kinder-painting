9c86204d5b9d397a26bc6aaa13a9b84d1c3a71d9
James Hamann <hamannjames87@gmail.com>
2024-01-17 18:51:22 -0800

    add trailing slash to links

---
+++ docs/_app/immutable/chunks/paths.4sBOrek6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_4uoik1?.base??"/kinder-painting",a=globalThis.__sveltekit_4uoik1?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/paths.Z0qfJahx.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_183hhg6?.base??"/kinder-painting",a=globalThis.__sveltekit_183hhg6?.assets??s;export{a,s as b};
