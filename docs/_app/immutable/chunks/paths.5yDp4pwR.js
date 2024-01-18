a6c5558094b28db5b02f814c2256274e5919608c
James Hamann <hamannjames87@gmail.com>
2024-01-17 18:49:05 -0800

    trailing slash option

--- docs/_app/immutable/chunks/paths.5yDp4pwR.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_9f1lm8?.base??"/kinder-painting",a=globalThis.__sveltekit_9f1lm8?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/chunks/paths.Z0qfJahx.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_183hhg6?.base??"/kinder-painting",a=globalThis.__sveltekit_183hhg6?.assets??s;export{a,s as b};
